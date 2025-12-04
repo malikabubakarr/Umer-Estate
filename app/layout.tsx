dd21c620ae8bea1a672fb506bd0c5a382d63fd71
Abubakar <abubakarrmalik383@gmail.com>
2025-12-05 01:55:04 +0500

    favicon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,21 @@ import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import BackButton from "@/components/BackButton";
 import Announcement from "@/components/Announcement"; // ← ADD THIS
+import {Metadata} from "next"; 
+import { PropsWithChildren } from "react";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "UmerEstate - Real Estate in Pakistan",
   description: "Best real estate listings, projects & investment opportunities",
+  icons: [
+    {
+      rel: "icon",
+      url: "/logo.png"
+    }
+  ]
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className="bg-white text-gray-900">
